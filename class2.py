8de54bbbe5c96966e30fc3e305ea69ea86ed9519
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-02 08:47:24 +0500

    first commit

---
+++ class2.py
@@ -0,0 +1,10 @@
+
+side1 = float(input("Enter the length of the first side: "))
+side2 = float(input("Enter the length of the second side: "))
+side3 = float(input("Enter the length of the third side: "))
+
+
+perimeter = side1 + side2 + side3
+
+
+print(f"The perimeter of the triangle is: {perimeter}")
